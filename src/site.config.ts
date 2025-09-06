86855f115da15e99bf2b395fc966444a41123056
Lar Van Der Jagt <lar@codeconfab.com>
2025-09-06 18:36:27 -0400

    Initial site config updates

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,16 +2,16 @@ import type { SiteConfig } from '~/types'
 
 const config: SiteConfig = {
   // Absolute URL to the root of your published site, used for generating links and sitemaps.
-  site: 'https://multiterm.stelclementine.com',
+  site: 'https://edgecompute.dev',
   // The name of your site, used in the title and for SEO.
-  title: 'MultiTerm',
+  title: 'edgecompute.dev',
   // The description of your site, used for SEO and RSS feed.
   description:
-    'A coder-ready Astro blog theme with 59 of your favorite color schemes to choose from',
+    'Dev blog for Lar Van Der Jagt',
   // The author of the site, used in the footer, SEO, and RSS feed.
-  author: 'Katy Kookaburra',
+  author: 'Lar Van Der Jagt',
   // Keywords for SEO, used in the meta tags.
-  tags: ['Astro', 'Terminal', 'Theme', 'MultiTerm', 'stelcodes'],
+  tags: ['Temporal', 'Edge Compute', 'Ruby', 'Buildkite'],
   // Path to the image used for generating social media previews.
   // Needs to be a square JPEG file due to limitations of the social card generator.
   // Try https://squoosh.app/ to easily convert images to JPEG.
@@ -42,87 +42,87 @@ const config: SiteConfig = {
     },
     {
       name: 'GitHub',
-      url: 'https://github.com/stelcodes/multiterm-astro',
+      url: 'https://github.com/codeconfab/edgecompute.dev',
       external: true,
     },
   ],
   // The theming configuration for the site.
   themes: {
     // The theming mode. One of "single" | "select" | "light-dark-auto".
-    mode: 'select',
+    mode: 'light-dark-auto',
     // The default theme identifier, used when themeMode is "select" or "light-dark-auto".
     // Make sure this is one of the themes listed in `themes` or "auto" for "light-dark-auto" mode.
-    default: 'catppuccin-mocha',
+    default: 'nord',
     // Shiki themes to bundle with the site.
     // https://expressive-code.com/guides/themes/#using-bundled-themes
     // These will be used to theme the entire site along with syntax highlighting.
     // To use light-dark-auto mode, only include a light and a dark theme in that order.
-    // include: [
-    //   'github-light',
-    //   'github-dark',
-    // ]
     include: [
-      'andromeeda',
-      'aurora-x',
-      'ayu-dark',
-      'catppuccin-frappe',
-      'catppuccin-latte',
-      'catppuccin-macchiato',
-      'catppuccin-mocha',
-      'dark-plus',
-      'dracula',
-      'dracula-soft',
-      'everforest-dark',
-      'everforest-light',
-      'github-dark',
-      'github-dark-default',
-      'github-dark-dimmed',
-      'github-dark-high-contrast',
-      'github-light',
-      'github-light-default',
-      'github-light-high-contrast',
-      'gruvbox-dark-hard',
-      'gruvbox-dark-medium',
-      'gruvbox-dark-soft',
-      'gruvbox-light-hard',
-      'gruvbox-light-medium',
-      'gruvbox-light-soft',
-      'houston',
-      'kanagawa-dragon',
-      'kanagawa-lotus',
-      'kanagawa-wave',
-      'laserwave',
-      'light-plus',
-      'material-theme',
-      'material-theme-darker',
-      'material-theme-lighter',
-      'material-theme-ocean',
-      'material-theme-palenight',
-      'min-dark',
-      'min-light',
-      'monokai',
-      'night-owl',
-      'nord',
-      'one-dark-pro',
-      'one-light',
-      'plastic',
-      'poimandres',
-      'red',
-      'rose-pine',
-      'rose-pine-dawn',
-      'rose-pine-moon',
-      'slack-dark',
-      'slack-ochin',
-      'snazzy-light',
-      'solarized-dark',
       'solarized-light',
-      'synthwave-84',
-      'tokyo-night',
-      'vesper',
-      'vitesse-black',
-      'vitesse-dark',
-      'vitesse-light',
+      'nord',
     ],
+    // include: [
+    //   'andromeeda',
+    //   'aurora-x',
+    //   'ayu-dark',
+    //   'catppuccin-frappe',
+    //   'catppuccin-latte',
+    //   'catppuccin-macchiato',
+    //   'catppuccin-mocha',
+    //   'dark-plus',
+    //   'dracula',
+    //   'dracula-soft',
+    //   'everforest-dark',
+    //   'everforest-light',
+    //   'github-dark',
+    //   'github-dark-default',
+    //   'github-dark-dimmed',
+    //   'github-dark-high-contrast',
+    //   'github-light',
+    //   'github-light-default',
+    //   'github-light-high-contrast',
+    //   'gruvbox-dark-hard',
+    //   'gruvbox-dark-medium',
+    //   'gruvbox-dark-soft',
+    //   'gruvbox-light-hard',
+    //   'gruvbox-light-medium',
+    //   'gruvbox-light-soft',
+    //   'houston',
+    //   'kanagawa-dragon',
+    //   'kanagawa-lotus',
+    //   'kanagawa-wave',
+    //   'laserwave',
+    //   'light-plus',
+    //   'material-theme',
+    //   'material-theme-darker',
+    //   'material-theme-lighter',
+    //   'material-theme-ocean',
+    //   'material-theme-palenight',
+    //   'min-dark',
+    //   'min-light',
+    //   'monokai',
+    //   'night-owl',
+    //   'nord',
+    //   'one-dark-pro',
+    //   'one-light',
+    //   'plastic',
+    //   'poimandres',
+    //   'red',
+    //   'rose-pine',
+    //   'rose-pine-dawn',
+    //   'rose-pine-moon',
+    //   'slack-dark',
+    //   'slack-ochin',
+    //   'snazzy-light',
+    //   'solarized-dark',
+    //   'solarized-light',
+    //   'synthwave-84',
+    //   'tokyo-night',
+    //   'vesper',
+    //   'vitesse-black',
+    //   'vitesse-dark',
+    //   'vitesse-light',
+    // ],
     // Optional overrides for specific themes to customize colors.
     // Their values can be either a literal color (hex, rgb, hsl) or another theme key.
     // See themeKeys list in src/types.ts for available keys to override and reference.
@@ -151,12 +151,11 @@ const config: SiteConfig = {
   },
   // Social links to display in the footer.
   socialLinks: {
-    github: 'https://github.com/stelcodes/multiterm-astro',
-    mastodon: 'https://github.com/stelcodes/multiterm-astro',
-    email: 'https://github.com/stelcodes/multiterm-astro',
-    linkedin: 'https://github.com/stelcodes/multiterm-astro',
-    bluesky: 'https://github.com/stelcodes/multiterm-astro',
-    twitter: 'https://github.com/stelcodes/multiterm-astro',
+    github: 'https://github.com/supaspoida',
+    mastodon: '',
+    email: 'lar@codeconfab.com',
+    linkedin: 'https://linkedin.com/in/supaspoida',
+    bluesky: '',
     rss: true, // Set to true to include an RSS feed link in the footer
   },
   // Configuration for Giscus comments.
@@ -166,10 +165,10 @@ const config: SiteConfig = {
   // IMPORTANT: Update giscus.json in the root of the project with your own website URL
   // If you don't want to use Giscus, set this to undefined.
   giscus: {
-    repo: 'stelcodes/multiterm-astro',
-    repoId: 'R_kgDOPNnBig',
+    repo: 'codeconfab/edgecompute.dev',
+    repoId: '',
     category: 'Giscus',
-    categoryId: 'DIC_kwDOPNnBis4CteOc',
+    categoryId: '',
     reactionsEnabled: true, // Enable reactions on post itself
   },
   // These are characters available for the character chat feature.
